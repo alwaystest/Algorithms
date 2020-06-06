d4638fb04084479c322988ae35b4190618324c06
eric <7553247+alwaystest@users.noreply.github.com>
2020-06-06 15:02:14 +0800

    ADD: largestIsland impl
    add test case for calculate

--- src/main/kotlin/problem827/Solution.kt
+++ src/main/kotlin/problem827/Solution.kt
@@ -1,5 +1,13 @@
 package problem827
 
 fun largestIsland(grid: Array<IntArray>): Int {
-    return 1
+    if (grid.size == 1) return 1
+    val n = grid.size
+    val index2Size = Array(n) { IntArray(n) }
+    calculate(grid, index2Size)
+    return index2Size.asSequence().map { it.max() }.maxBy { it!! }!!
+}
+
+fun calculate(grid: Array<IntArray>, index2Size: Array<IntArray>) {
+    TODO()
 }
--- src/test/kotlin/problem827/SolutionKtTest.kt
+++ src/test/kotlin/problem827/SolutionKtTest.kt
@@ -1,6 +1,7 @@
 package problem827
 
 import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.core.Is
 import org.junit.Test
 
@@ -57,5 +58,67 @@ class SolutionKtTest(private val data: Array<IntArray>, private val expect: Int)
     @Test
     fun largestIsland() {
         largestIsland(data) shouldBe expect
+    }
+}
+
+@RunWith(Parameterized::class)
+class CalculateTest(private val data: Array<IntArray>, private val expect: Array<IntArray>) {
+    companion object {
+        @Parameterized.Parameters
+        @JvmStatic
+        fun params() = listOf(
+            arrayOf(
+                arrayOf(
+                    intArrayOf(0, 0),
+                    intArrayOf(0, 0)
+                ), arrayOf(
+                    intArrayOf(1, 1),
+                    intArrayOf(1, 1)
+                )
+            ),
+            arrayOf(
+                arrayOf(
+                    intArrayOf(1, 0),
+                    intArrayOf(0, 0)
+                ), arrayOf(
+                    intArrayOf(1, 2),
+                    intArrayOf(2, 1)
+                )
+            ),
+            arrayOf(
+                arrayOf(
+                    intArrayOf(1, 1),
+                    intArrayOf(0, 0)
+                ), arrayOf(
+                    intArrayOf(2, 2),
+                    intArrayOf(3, 3)
+                )
+            ),
+            arrayOf(
+                arrayOf(
+                    intArrayOf(1, 1),
+                    intArrayOf(1, 0)
+                ), arrayOf(
+                    intArrayOf(3, 3),
+                    intArrayOf(3, 4)
+                )
+            ),
+            arrayOf(
+                arrayOf(
+                    intArrayOf(1, 1),
+                    intArrayOf(1, 1)
+                ), arrayOf(
+                    intArrayOf(4, 4),
+                    intArrayOf(4, 4)
+                )
+            )
+        )
+    }
+
+    @Test
+    fun calculate() {
+        val result = Array(data.size) { IntArray(data.size) }
+        calculate(data, result)
+        assertThat(result, equalTo(expect))
     }
 }
