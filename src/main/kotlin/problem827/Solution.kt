2ac62eba6cbada223789dfaf00c51a62ea685d5d
eric <7553247+alwaystest@users.noreply.github.com>
2020-06-06 23:52:02 +0800

    FIX: calculate
    according to test case

--- src/main/kotlin/problem827/Solution.kt
+++ src/main/kotlin/problem827/Solution.kt
@@ -14,6 +14,7 @@ fun calculate(grid: Array<IntArray>, index2Size: Array<IntArray>) {
     for (x in 0 until n) {
         for (y in 0 until n) {
             index2Size[x][y] = dfs(grid, x to y, searched)
+            searched.clear()
         }
     }
 }
@@ -25,9 +26,11 @@ private fun dfs(
 ): Int {
     // TODO(optimise: index2Size as cache?)
     var result = 0
+    // if searched, return 0
+    if (index in searched) return result
+    val (x, y) = index
     // if is first item, 1 --> 1 + sum(neighbors), 0 --> 1 + sum(neighbors)
     // if not first item, 1 --> 1 + sum(neighbors), 0 --> 0
-    val (x, y) = index
     if (searched.isNotEmpty() && grid[x][y] == 0) {
         return 0
     }
@@ -38,7 +41,6 @@ private fun dfs(
     neighbors.forEach {
         result += dfs(grid, it, searched)
     }
-    searched.remove(index)
 
     return result
 }
