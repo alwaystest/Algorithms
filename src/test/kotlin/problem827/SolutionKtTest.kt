c01089fb7bde2719c74e4a325c032f32289746f3
eric <7553247+alwaystest@users.noreply.github.com>
2020-06-06 14:27:24 +0800

    ADD: test case for 827

--- src/test/kotlin/problem827/SolutionKtTest.kt
+++ src/test/kotlin/problem827/SolutionKtTest.kt
@@ -5,17 +5,57 @@ import org.hamcrest.core.Is
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 
 infix fun <T> T.shouldBe(t: T) = assertThat(this, `is`(t))
 
-class SolutionKtTest {
+@RunWith(Parameterized::class)
+class SolutionKtTest(private val data: Array<IntArray>, private val expect: Int) {
+    companion object {
+        @Parameterized.Parameters
+        @JvmStatic
+        fun params() = listOf(
+            arrayOf(
+                arrayOf(
+                    intArrayOf(0)
+                ), 1
+            ),
+            arrayOf(
+                arrayOf(
+                    intArrayOf(0, 0),
+                    intArrayOf(0, 0)
+                ), 1
+            ),
+            arrayOf(
+                arrayOf(
+                    intArrayOf(1, 0),
+                    intArrayOf(0, 0)
+                ), 2
+            ),
+            arrayOf(
+                arrayOf(
+                    intArrayOf(1, 1),
+                    intArrayOf(0, 0)
+                ), 3
+            ),
+            arrayOf(
+                arrayOf(
+                    intArrayOf(1, 1),
+                    intArrayOf(3, 0)
+                ), 4
+            ),
+            arrayOf(
+                arrayOf(
+                    intArrayOf(1, 1),
+                    intArrayOf(1, 1)
+                ), 4
+            )
+        )
+    }
 
     @Test
     fun largestIsland() {
-        val data = arrayOf(
-            intArrayOf(0, 0),
-            intArrayOf(0, 0)
-        )
-        largestIsland(data) shouldBe 1
+        largestIsland(data) shouldBe expect
     }
 }
