1363a73bc66bb6f8be6370eef761a5155e508741
eric <7553247+alwaystest@users.noreply.github.com>
2020-06-13 15:31:06 +0800

    MOD: optimise Solution239

--- src/main/kotlin/problem239/Solution239.kt
+++ src/main/kotlin/problem239/Solution239.kt
@@ -7,15 +7,20 @@ fun maxSlidingWindow(nums: IntArray, k: Int): IntArray {
     if (k == 1) {
         return nums
     }
-    val window = LinkedList<Int>()
+    val window = TreeMap<Int, Int>()
     val result = ArrayList<Int>(nums.size - k + 1)
     nums.forEachIndexed { index, i ->
-        window.addFirst(i)
+        window[i] = (window[i] ?: 0) + 1
         if (index >= k) {
-            window.removeLast()
+            val last = nums[index - k]
+            if (window[last] == 1) {
+                window.remove(last)
+            } else {
+                window[last] = window[last]!! - 1
+            }
         }
         if (index >= k - 1) {
-            result.add(window.max()!!)
+            result.add(window.lastKey())
         }
     }
     return result.toIntArray()
