c3cdcf494c0841e4a9f0880d8456eb9d40e0edf0
eric <7553247+alwaystest@users.noreply.github.com>
2020-06-13 14:02:44 +0800

    ADD: Solution239 impl

--- src/main/kotlin/problem239/Solution239.kt
+++ src/main/kotlin/problem239/Solution239.kt
@@ -1,5 +1,22 @@
 package problem239
 
+import java.util.*
+
 fun maxSlidingWindow(nums: IntArray, k: Int): IntArray {
-    TODO("Impl")
+    assert(nums.size >= k)
+    if (k == 1) {
+        return nums
+    }
+    val window = LinkedList<Int>()
+    val result = ArrayList<Int>(nums.size - k + 1)
+    nums.forEachIndexed { index, i ->
+        window.addFirst(i)
+        if (index >= k) {
+            window.removeLast()
+        }
+        if (index >= k - 1) {
+            result.add(window.max()!!)
+        }
+    }
+    return result.toIntArray()
 }
