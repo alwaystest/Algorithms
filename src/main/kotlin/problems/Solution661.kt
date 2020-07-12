3438a5ca86a4b162a45d3ae6eaf62a0447dfe884
eric <7553247+alwaystest@users.noreply.github.com>
2020-07-12 16:35:59 +0800

    ADD: Solution661 impl and UnitTests

--- src/main/kotlin/problems/Solution661.kt
+++ src/main/kotlin/problems/Solution661.kt
@@ -1,5 +1,56 @@
 package problems
 
+import kotlin.math.floor
+
 fun imageSmoother(M: Array<IntArray>): Array<IntArray> {
-    TODO()
+    if (M.isEmpty()) return M
+    val result = Array(M.size) {
+        IntArray(M[it].size)
+    }
+    for (x in M.indices) {
+        for (y in M[x].indices) {
+            val average = getAverage(M, x, y)
+            result[x][y] = average
+        }
+    }
+    return result
+}
+
+fun getAverage(m: Array<IntArray>, x: Int, y: Int): Int {
+    val neighbors = getNeighbors(m, x, y)
+    return floor(
+        neighbors.run {
+            add(m[x][y])
+            average()
+        }
+    ).toInt()
+}
+
+fun getNeighbors(m: Array<IntArray>, x: Int, y: Int): MutableList<Int> {
+    val result = mutableListOf<Int>()
+    validateAndAdd(m, x - 1, y - 1, result)
+    validateAndAdd(m, x - 1, y, result)
+    validateAndAdd(m, x - 1, y + 1, result)
+    validateAndAdd(m, x, y - 1, result)
+    validateAndAdd(m, x, y + 1, result)
+    validateAndAdd(m, x + 1, y - 1, result)
+    validateAndAdd(m, x + 1, y, result)
+    validateAndAdd(m, x + 1, y + 1, result)
+    return result
+}
+
+fun validateAndAdd(m: Array<IntArray>, x: Int, y: Int, result: MutableList<Int>) {
+    if (isValid(m, x, y)) {
+        result.add(m[x][y])
+    }
+}
+
+fun isValid(m: Array<IntArray>, x: Int, y: Int): Boolean {
+    if (x < 0 || x >= m.size) {
+        return false
+    }
+    if (y < 0 || y >= m[x].size) {
+        return false
+    }
+    return true
 }
--- src/test/kotlin/problems/Solution661Test.kt
+++ src/test/kotlin/problems/Solution661Test.kt
@@ -30,4 +30,71 @@ class Solution661Test {
             }
         }
     }
+
+    @Test
+    fun isValid_always_verifyResult() {
+        val array = arrayOf(
+            intArrayOf(1)
+        )
+
+        val invalid = listOf(
+            isValid(array, -1, -1),
+            isValid(array, -1, 0),
+            isValid(array, -1, 1),
+            isValid(array, 0, -1),
+            isValid(array, 0, 1),
+            isValid(array, 1, -1),
+            isValid(array, 1, 0),
+            isValid(array, 1, 1)
+        )
+        val valid = isValid(array, 0, 0)
+
+        invalid.any { it } shouldBe false
+        valid shouldBe true
+    }
+
+    @Test
+    fun getNeighbors_noNeighbor_verifyResult() {
+        val array = arrayOf(
+            intArrayOf(1)
+        )
+
+        val neighbors = getNeighbors(array, 0, 0)
+
+        neighbors.isEmpty() shouldBe true
+    }
+
+    @Test
+    fun getNeighbors_oneLine_verifyResult() {
+        val array = arrayOf(
+            intArrayOf(1, 2)
+        )
+
+        val neighbors = getNeighbors(array, 0, 0)
+
+        neighbors.size shouldBe 1
+        neighbors[0] shouldBe 2
+    }
+
+    @Test
+    fun getAverage_onNeighbor_shouldBeSelf() {
+        val array = arrayOf(
+            intArrayOf(1)
+        )
+
+        val avg = getAverage(array, 0, 0)
+
+        avg shouldBe 1
+    }
+
+    @Test
+    fun getAverage_oneLine_shouldBeSelf() {
+        val array = arrayOf(
+            intArrayOf(1, 3)
+        )
+
+        val avg = getAverage(array, 0, 0)
+
+        avg shouldBe 2
+    }
 }
