f0d080f7ae6a6784f1ce08191e5f49e3361ccc51
eric <7553247+alwaystest@users.noreply.github.com>
2020-07-19 00:27:52 +0800

    ADD: test case for 199

--- src/test/kotlin/problems/Solution199KtTest.kt
+++ src/test/kotlin/problems/Solution199KtTest.kt
@@ -9,7 +9,7 @@ class Solution199KtTest {
     @Test
     fun rightSideView_null_returnEmptyList() {
         val result = rightSideView(null)
-         result.size shouldBe 0
+        result.size shouldBe 0
     }
 
     @Test
@@ -17,6 +17,38 @@ class Solution199KtTest {
         val result = rightSideView(TreeNode(1))
 
         (result == listOf(1)) shouldBe true
+    }
+
+    @Test
+    fun rightSideView_twoNodes_verifyResult() {
+        val root = TreeNode(1)
+        root.left = TreeNode(2)
+
+        val result = rightSideView(root)
+
+        (result == listOf(1, 2)) shouldBe true
+    }
+
+    @Test
+    fun rightSideView_threeNodes_verifyResult() {
+        val root = TreeNode(1)
+        root.left = TreeNode(2)
+        root.right = TreeNode(3)
+
+        val result = rightSideView(root)
+
+        (result == listOf(1, 3)) shouldBe true
+    }
+
+    @Test
+    fun rightSideView_threeNodesLeft_verifyResult() {
+        val root = TreeNode(1)
+        root.left = TreeNode(2)
+        root.left?.left = TreeNode(3)
+
+        val result = rightSideView(root)
+
+        (result == listOf(1, 2, 3)) shouldBe true
     }
 
 }
