a6dd706deac95ce91cdaefabc1678ab5a8d7b05c
eric <7553247+alwaystest@users.noreply.github.com>
2020-08-09 13:09:30 +0800

    ADD: validateCircle via DFS

--- src/main/kotlin/problems/Solution1361.kt
+++ src/main/kotlin/problems/Solution1361.kt
@@ -6,8 +6,21 @@ fun validateBinaryTreeNodes(n: Int, leftChild: IntArray, rightChild: IntArray): 
         if (!addParentCountAndValidate(leftChild[i], parentCountArray)) return false
         if (!addParentCountAndValidate(rightChild[i], parentCountArray)) return false
     }
-    val rootCount = parentCountArray.count { it == 0 }
-    return rootCount == 1
+    var rootCount = 0
+    var root = -1
+    parentCountArray.forEachIndexed { index, parentCount ->
+        if (parentCount == 0) {
+            rootCount++
+            root = index
+            if (rootCount > 1) return false
+        }
+    }
+    if (root == -1) return false
+    val visited = BooleanArray(n)
+    visited[root] = true
+    if (!validateCircle(leftChild[root], leftChild, rightChild, visited)) return false
+    if (!validateCircle(rightChild[root], leftChild, rightChild, visited)) return false
+    return visited.all { it }
 }
 
 fun addParentCountAndValidate(node: Int, parentCountArray: IntArray): Boolean {
@@ -16,4 +29,12 @@ fun addParentCountAndValidate(node: Int, parentCountArray: IntArray): Boolean {
         if (parentCountArray[node] > 1) return false
     }
     return true
+}
+
+fun validateCircle(node: Int, leftChildren: IntArray, rightChildren: IntArray, visited: BooleanArray): Boolean {
+    if (node == -1) return true
+    if (visited[node]) return false
+    visited[node] = true
+    return validateCircle(leftChildren[node], leftChildren, rightChildren, visited) &&
+            validateCircle(rightChildren[node], leftChildren, rightChildren, visited)
 }
--- src/test/kotlin/problems/Solution1361KtTest.kt
+++ src/test/kotlin/problems/Solution1361KtTest.kt
@@ -30,6 +30,24 @@ class Solution1361KtTest {
     }
 
     @Test
+    fun validateBinaryTreeNodes_circleCase_invalid() {
+        val result = validateBinaryTreeNodes(4, intArrayOf(1, 2, 0, -1), intArrayOf(-1, -1, -1, -1))
+        result shouldBe false
+    }
+
+    @Test
+    fun validateBinaryTreeNodes_circleCase1_invalid() {
+        val result = validateBinaryTreeNodes(2, intArrayOf(0, -1), intArrayOf(-1, -1))
+        result shouldBe false
+    }
+
+    @Test
+    fun validateBinaryTreeNodes_firstNotRoot_invalid() {
+        val result = validateBinaryTreeNodes(2, intArrayOf(-1, 0), intArrayOf(-1, -1))
+        result shouldBe true
+    }
+
+    @Test
     fun parentCountValid_count0_valid() {
         addParentCountAndValidate(0, intArrayOf(0)) shouldBe true
     }
@@ -42,5 +60,15 @@ class Solution1361KtTest {
     @Test
     fun parentCountValid_moreThan1_invalid() {
         addParentCountAndValidate(0, intArrayOf(2)) shouldBe false
+    }
+
+    @Test
+    fun validateCircle_noCircle_valid() {
+        validateCircle(0, intArrayOf(1, -1), intArrayOf(-1, -1), BooleanArray(2)) shouldBe true
+    }
+
+    @Test
+    fun validateCircle_circle_invalid() {
+        validateCircle(0, intArrayOf(1, 0), intArrayOf(-1, -1), BooleanArray(2)) shouldBe false
     }
 }
