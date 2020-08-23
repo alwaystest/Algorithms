046e3a1875ca190f36224deddb81b24be43db054
eric <7553247+alwaystest@users.noreply.github.com>
2020-08-23 12:12:31 +0800

    ADD: buildGraph

--- src/main/kotlin/problems/Solution11229.kt
+++ src/main/kotlin/problems/Solution11229.kt
@@ -2,5 +2,26 @@ package problems
 
 fun shortestAlternatingPaths(n: Int, red_edges: Array<IntArray>, blue_edges: Array<IntArray>): IntArray {
     val result = IntArray(n) { -1 }
+    val graph = buildGraph(n, red_edges, blue_edges)
     return result
 }
+
+fun buildGraph(
+    n: Int,
+    red_edges: Array<IntArray>,
+    blue_edges: Array<IntArray>
+): ArrayList<Node> {
+    val graph = arrayListOf<Node>()
+    for (i in 0 until n) {
+        graph.add(Node(i, mutableListOf(), mutableListOf()))
+    }
+    red_edges.forEach {
+        graph[it[0]].redNode.add(graph[it[1]])
+    }
+    blue_edges.forEach {
+        graph[it[0]].blueNode.add(graph[it[1]])
+    }
+    return graph
+}
+
+data class Node(val value: Int, val blueNode: MutableList<Node>, val redNode: MutableList<Node>)
--- src/test/kotlin/problems/Solution11229KtTest.kt
+++ src/test/kotlin/problems/Solution11229KtTest.kt
@@ -7,7 +7,16 @@ import shouldBe
 class Solution11229KtTest {
 
     @Test
-    fun shortestAlternatingPaths_inputN_result_lengthN() {
+    fun shortestAlternatingPaths_inputN_resultLengthN() {
         shortestAlternatingPaths(3, arrayOf(), arrayOf()).size shouldBe 3
+    }
+
+    @Test
+    fun buildGraph_always_verifyGraph() {
+        val graph = buildGraph(3, arrayOf(intArrayOf(0, 1)), arrayOf(intArrayOf(1, 2)))
+
+        graph.size shouldBe 3
+        graph[0].redNode shouldBe listOf(graph[1])
+        graph[1].blueNode shouldBe listOf(graph[2])
     }
 }
