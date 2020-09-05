561d04826f8d42794ce2fdbe3517ee808e0e48f7
eric <7553247+alwaystest@users.noreply.github.com>
2020-09-05 22:45:06 +0800

    ADD: September Challenge 5

---
+++ src/main/kotlin/challenge/september/week1/Solution5.kt
@@ -0,0 +1,23 @@
+package challenge.september.week1
+
+// All Elements in Two Binary Search Trees
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun getAllElements(root1: TreeNode?, root2: TreeNode?): List<Int> {
+        TODO()
+    }
+}
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
