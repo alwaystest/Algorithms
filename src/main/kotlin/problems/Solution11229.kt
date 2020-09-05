c11492d932228f12d7a35e856fa5ff29fb30c53a
eric <7553247+alwaystest@users.noreply.github.com>
2020-09-05 15:41:32 +0800

    ADD: complete Solution1129

--- src/main/kotlin/problems/Solution11229.kt
+++ src/main/kotlin/problems/Solution11229.kt
@@ -1,9 +1,61 @@
 package problems
 
+import java.util.*
+import kotlin.collections.ArrayList
+
 fun shortestAlternatingPaths(n: Int, red_edges: Array<IntArray>, blue_edges: Array<IntArray>): IntArray {
     val result = IntArray(n) { -1 }
     val graph = buildGraph(n, red_edges, blue_edges)
+    wfs(graph ,result)
     return result
+}
+
+fun wfs(graph: ArrayList<Node>, result: IntArray) {
+    if (graph.isEmpty()) return
+    val path = IntArray(2 * result.size)
+    var queue = LinkedList<Pair<Node, Boolean>>()
+    queue.add(graph[0] to true)
+    queue.add(graph[0] to false)
+    var depth = 0
+    while (!queue.isEmpty()) {
+        queue = wfsImpl(depth++, result, path, queue)
+    }
+}
+
+fun wfsImpl(
+    depth: Int,
+    result: IntArray,
+    path: IntArray,
+    queue: LinkedList<Pair<Node, Boolean>>
+) :LinkedList<Pair<Node, Boolean>> {
+    val nextLevel = LinkedList<Pair<Node, Boolean>>()
+    queue.forEach { (node, chooseRed) ->
+        if (chooseRed) {
+            // next choose red, this is form blue path
+            path[node.value * 2 + 1] = 1
+        } else {
+            path[node.value * 2] = 1
+        }
+
+        if (result[node.value] == -1) {
+            result[node.value] = depth
+        }
+
+        if (chooseRed) {
+            node.redNode.forEach {
+                if (path[it.value * 2] == 0) {
+                    nextLevel.add(it to false)
+                }
+            }
+        } else {
+            node.blueNode.forEach {
+                if (path[it.value * 2 + 1] == 0) {
+                    nextLevel.add(it to true)
+                }
+            }
+        }
+    }
+    return nextLevel
 }
 
 fun buildGraph(
--- src/test/kotlin/problems/Solution11229KtTest.kt
+++ src/test/kotlin/problems/Solution11229KtTest.kt
@@ -12,11 +12,70 @@ class Solution11229KtTest {
     }
 
     @Test
+    fun shortestAlternatingPaths_example1_verifyResult() {
+        val result = shortestAlternatingPaths(3, arrayOf(intArrayOf(0, 1), intArrayOf(1, 2)), arrayOf())
+
+        result shouldBe intArrayOf(0, 1, -1)
+    }
+
+    @Test
+    fun shortestAlternatingPaths_example2_verifyResult() {
+        val result = shortestAlternatingPaths(3, arrayOf(intArrayOf(0, 1)), arrayOf(intArrayOf(2, 1)))
+
+        result shouldBe intArrayOf(0, 1, -1)
+    }
+
+    @Test
+    fun shortestAlternatingPaths_example3_verifyResult() {
+        val result = shortestAlternatingPaths(3, arrayOf(intArrayOf(1, 0)), arrayOf(intArrayOf(2, 1)))
+
+        result shouldBe intArrayOf(0, -1, -1)
+    }
+
+    @Test
+    fun shortestAlternatingPaths_example4_verifyResult() {
+        val result = shortestAlternatingPaths(3, arrayOf(intArrayOf(0, 1)), arrayOf(intArrayOf(1, 2)))
+
+        result shouldBe intArrayOf(0, 1, 2)
+    }
+
+    @Test
+    fun shortestAlternatingPaths_example5_verifyResult() {
+        val result = shortestAlternatingPaths(3, arrayOf(intArrayOf(0, 1), intArrayOf(0, 2)), arrayOf(intArrayOf(1, 0)))
+
+        result shouldBe intArrayOf(0, 1, 1)
+    }
+
+    @Test
+    fun shortestAlternatingPaths_example6_verifyResult() {
+        val result = shortestAlternatingPaths(
+            5,
+            arrayOf(intArrayOf(0, 1), intArrayOf(1, 2), intArrayOf(2, 3), intArrayOf(3, 4)),
+            arrayOf(intArrayOf(1, 2), intArrayOf(2, 3), intArrayOf(3, 1))
+        )
+
+        result shouldBe intArrayOf(0, 1, 2, 3, 7)
+    }
+
+    @Test
     fun buildGraph_always_verifyGraph() {
         val graph = buildGraph(3, arrayOf(intArrayOf(0, 1)), arrayOf(intArrayOf(1, 2)))
 
         graph.size shouldBe 3
         graph[0].redNode shouldBe listOf(graph[1])
         graph[1].blueNode shouldBe listOf(graph[2])
+    }
+
+    @Test
+    fun wfs_always_verifyResult() {
+        val n2 = Node(2, mutableListOf(), mutableListOf())
+        val n1 = Node(1, mutableListOf(), mutableListOf(n2))
+        val n0 = Node(0, mutableListOf(n1), mutableListOf())
+        val graph = arrayListOf(n0, n1, n2)
+        val result = IntArray(3) { -1 }
+
+        wfs(graph, result)
+
+        result shouldBe intArrayOf(0, 1, 2)
     }
 }
