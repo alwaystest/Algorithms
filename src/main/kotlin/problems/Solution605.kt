35151aa29f67414d922de359e522c4202a90c781
eric <7553247+alwaystest@users.noreply.github.com>
2020-09-20 23:06:07 +0800

    ADD: impl Solution605

--- src/main/kotlin/problems/Solution605.kt
+++ src/main/kotlin/problems/Solution605.kt
@@ -1,5 +1,17 @@
 package problems
 
 fun canPlaceFlowers(flowerbed: IntArray, n: Int): Boolean {
-    TODO()
+    var count = 0
+    var i = 0
+    while (i < flowerbed.size) {
+        if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.size - 1 || flowerbed[i + 1] == 0)) {
+            flowerbed[i] = 1
+            count++
+        }
+        i++
+        if (count >= n) {
+            return true
+        }
+    }
+    return false
 }
