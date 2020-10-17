864a2eae46ada045c92ca2b4f6832e7d6fc345e2
eric <7553247+alwaystest@users.noreply.github.com>
2020-10-17 11:35:33 +0800

    MOD: optimize Solution53

--- src/main/kotlin/problems/Solution53.kt
+++ src/main/kotlin/problems/Solution53.kt
@@ -7,11 +7,7 @@ fun maxSubArray(nums: IntArray): Int {
     var sum = nums[0]
     var maxSum = sum
     for (i in 1 until nums.size) {
-        if (sum < 0 && nums[i] > sum) {
-            sum = nums[i]
-        } else {
-            sum += nums[i]
-        }
+        sum = max(nums[i], sum + nums[i])
         maxSum = max(maxSum, sum)
     }
     return maxSum
