f00fee35197db926a1858a827b26006904cbff99
eric <7553247+alwaystest@users.noreply.github.com>
2020-11-05 15:01:38 +0800

    ADD: test case for Solution91

---
+++ src/test/kotlin/problems/Solution91KtTest.kt
@@ -0,0 +1,28 @@
+package problems
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import shouldBe
+
+@RunWith(Parameterized::class)
+class Solution91KtTest(private val s: String, private val result: Int) {
+
+    companion object {
+
+        @JvmStatic
+        @Parameterized.Parameters
+        fun params() = listOf(
+            arrayOf("0", 0),
+            arrayOf("1", 1),
+            arrayOf("12", 2),
+            arrayOf("226", 3),
+            arrayOf("227", 2)
+        )
+    }
+
+    @Test
+    fun numDecodings_params_verifyResult() {
+        numDecodings(s) shouldBe result
+    }
+}
