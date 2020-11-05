0dd078fad622947903351a3fb47488886fd85a88
eric <7553247+alwaystest@users.noreply.github.com>
2020-11-05 18:40:27 +0800

    FIX: Solution91
    update test case and fix solution

--- src/test/kotlin/problems/Solution91KtTest.kt
+++ src/test/kotlin/problems/Solution91KtTest.kt
@@ -16,9 +16,12 @@ class Solution91KtTest(private val s: String, private val result: Int) {
             arrayOf("0", 0),
             arrayOf("1", 1),
             arrayOf("10", 1),
+            arrayOf("11", 2),
             arrayOf("12", 2),
             arrayOf("226", 3),
-            arrayOf("227", 2)
+            arrayOf("227", 2),
+            arrayOf("1201234", 3),
+            arrayOf("2101", 1)
         )
     }
 
