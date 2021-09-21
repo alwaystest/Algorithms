3646635bf54bc6b05fd35b5bfa3552930fd04d07
eric <7553247+alwaystest@users.noreply.github.com>
2021-09-21 11:19:43 +0800

    ADD: impl of Solution350

--- src/main/kotlin/problems/Solution350.kt
+++ src/main/kotlin/problems/Solution350.kt
@@ -1,5 +1,19 @@
 package problems
 
 fun intersect(nums1: IntArray, nums2: IntArray): IntArray {
-    TODO()
+    val map = mutableMapOf<Int, Int>()
+    nums1.forEach {
+        val count = map.getOrDefault(it, 0)
+        map[it] = count + 1
+    }
+
+    val result = mutableListOf<Int>()
+    nums2.forEach {
+        val count = map.getOrDefault(it, 0)
+        if (count != 0) {
+            result.add(it)
+            map[it] = count - 1
+        }
+    }
+    return result.toIntArray()
 }
